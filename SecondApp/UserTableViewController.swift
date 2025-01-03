c405735e0412c82d25c47ff0af423109afb6a1ce
Chahyun Park <clairebluepark@gmail.com>
2025-01-03 16:32:59 +0900

    테이블뷰 컨트롤러 커스텀 셀 구현

--- SecondApp/UserTableViewController.swift
+++ SecondApp/UserTableViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class UserTableViewController: UITableViewController {
+    
+    let name = ["칸쵸", "칙촉", "쿠크다스", "빼빼로", "씨리얼", "초코송이"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return name.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserTableViewCell
+        
+        cell.profileImageView.backgroundColor = .blue
+        cell.nameLabel.text = name[indexPath.row]
+        cell.messageLabel.text = "상태 메시지"
+        
+        cell.nameLabel.font = .boldSystemFont(ofSize: 15)
+        cell.messageLabel.font = .systemFont(ofSize: 12)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 
 }
